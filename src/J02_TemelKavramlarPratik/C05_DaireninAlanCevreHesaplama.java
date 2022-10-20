f9829b7684be680b88d7dc4f160d4f47a666805a
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-20 12:48:45 +0300

    20.10.22

--- src/J02_TemelKavramlarPratik/C05_DaireninAlanCevreHesaplama.java
+++ src/J02_TemelKavramlarPratik/C05_DaireninAlanCevreHesaplama.java
@@ -1,7 +1,17 @@
 package J02_TemelKavramlarPratik;
-
+import java.util.Scanner;
 public class C05_DaireninAlanCevreHesaplama {
     public static void main(String[] args) {
+        int r ;
+        double pi= 3.14;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter radius  :");
+        r = scanner.nextInt();
+        double perimeter = 2*pi*r;
+        double area =r*r*pi;
+        System.out.println("Circle  area      = " + area);
+        System.out.println("Circle  perimeter = " + perimeter);
+
 
     }
 }
