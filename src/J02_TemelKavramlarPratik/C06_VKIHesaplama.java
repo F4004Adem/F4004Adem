b05c0b3c17476ac9cb88e63cc4ac53b1f26d5385
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-20 18:13:26 +0300

    20.10.22

--- src/J02_TemelKavramlarPratik/C06_VKIHesaplama.java
+++ src/J02_TemelKavramlarPratik/C06_VKIHesaplama.java
@@ -1,7 +1,24 @@
 package J02_TemelKavramlarPratik;
 
+import java.util.Scanner;
+
 public class C06_VKIHesaplama {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen Boyunuzu giriniz metre cinsinden : ");
+        double boy = scanner.nextDouble();
+        System.out.print("Lutfen kilonuzu giriniz kg cinsinden :");
+        double kilo = scanner.nextDouble();
+        double VKI = kilo/(boy * boy);
+                if (VKI<20){
+                    System.out.println("cok zayifsiniz \nVKI= " + VKI);
+                }else if (20<=VKI && VKI<25){
+                    System.out.println("ideal kilodasiniz \nVKI= " + VKI);
+                }else if (25<=VKI && VKI<30){
+                    System.out.println("cok kilolusunuz \nVKI= " + VKI);
+                }else
+                    System.out.println("obezitesiniz  \nVKI= " + VKI);
 
     }
 }
