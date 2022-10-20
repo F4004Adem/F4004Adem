4bdd4da8be3fe05a8d7cd572feb1650dd4d64689
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-20 04:00:05 +0300

    20.10.22

--- src/J02_Pratik/C01_GradeCalc.java
+++ src/J02_Pratik/C01_GradeCalc.java
@@ -4,39 +4,30 @@ import java.util.Scanner;
 
 public class C01_GradeCalc {
     public static void main(String[] args) {
-// Degiskenler
-        int mat, fizik, kimya,turkce, tarih, muzik;
+        int math, physics, chemistry, history, music;
 
-        // Scanner sinifimizi tanimladik
         Scanner scan = new Scanner(System.in);
-
         //Kullanicidandegerler alinabilinir
-        System.out.print("Matematik Notunuz ");
-        mat = scan.nextInt();
+        System.out.print("Please enter your Math grade ");
+        math = scan.nextInt();
 
+        System.out.print("Please enter your Physics grade ");
+        physics = scan.nextInt();
 
-        System.out.print("Fizik Notunuz ");
-        fizik = scan.nextInt();
+        System.out.print("Please enter your Chemistry grade");
+        chemistry = scan.nextInt();
 
+        System.out.print("Please enter your History grade ");
+        history = scan.nextInt();
 
-        System.out.print("Kimya Notunuz ");
-        kimya = scan.nextInt();
+        System.out.print("Please enter your Music grade ");
+        music = scan.nextInt();
 
-
-        System.out.print("Turkce Notunuz ");
-        turkce = scan.nextInt();
-
-        System.out.print("Tarih Notunuz ");
-        tarih = scan.nextInt();
-
-        System.out.print("Muzik Notunuz ");
-        muzik = scan.nextInt();
-
-        double toplam = (mat+fizik+kimya+turkce+tarih+muzik);
-        double sonuc = toplam/6.0;
-        System.out.println(sonuc);
-        System.out.println("Gecti = " + (sonuc>=60));
-        System.out.println( (sonuc>=60)? "Gecti" : "Kaldi");
+        double toplam = (math + physics + chemistry + history +music);
+        double result = toplam/5.0;
+        System.out.println(result);
+        System.out.println("Did you Pass = " + (result>=60));
+        System.out.println( (result>=60)? "Congratulation You Passed" : "I'm Sorry but You Failed");
 
 
     }
