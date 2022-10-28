86ba99b050f8fbd2ea1f06ab4b1bc714f834e1b7
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-28 13:56:57 +0300

    28.10.22

--- src/J03_KosulluIfadelerVeKodBloklari/C04_SwitchCaseYapisi.java
+++ src/J03_KosulluIfadelerVeKodBloklari/C04_SwitchCaseYapisi.java
@@ -1,7 +1,86 @@
 package J03_KosulluIfadelerVeKodBloklari;
 
+import java.util.Scanner;
+
 public class C04_SwitchCaseYapisi {
     public static void main(String[] args) {
+        int day =2;
+        switch(day) {
+            case 1:
+                System.out.println(" Bugun = Pazartesi ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 2:System.out.println(" Bugun = Sali ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 3:System.out.println(" Bugun = Carsamba ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 4:System.out.println(" Bugun = Persembe ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 5:System.out.println(" Bugun = Cuma ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 6:System.out.println(" Bugun = Cumartesi ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 7:System.out.println(" Bugun = Pazar ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+
+            default:System.out.println(" Hatali giris yaptiniz ");
+                // değer hiç bir şeye eşit değilse bu kod bloğu çalışacak
+        }
+
+
+        // Kullanicidan Alma ile oluyor
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen gunu numara olarak giriniz ");
+        int value = input.nextInt();
+        switch (value) {
+            case 1:
+                System.out.println(" Bugun = Pazartesi ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 2:System.out.println(" Bugun = Sali ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 3:System.out.println(" Bugun = Carsamba ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 4:System.out.println(" Bugun = Persembe ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 5:System.out.println(" Bugun = Cuma ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+            case 6:System.out.println(" Bugun = Cumartesi ");
+                // değer y'ye eşitse bu kod bloğu çalışacak
+                break;
+            case 7:System.out.println(" Bugun = Pazar ");
+                // değer x'e eşitse bu kod bloğu çalışacak
+                break;
+
+            default:System.out.println(" Hatali giris yaptiniz ");
+                // değer hiç bir şeye eşit değilse bu kod bloğu çalışacak
+        }
+
+
+
+
+        int points=6;
+        switch(points)
+        {
+            case 6: ;
+            case 7: System.out.println("Java");break;
+            case 8: ;
+            case 9: System.out.println("101");break;
+            case 10: System.out.println("Patika"); break;
+            default: System.out.println("Dev");
+        }
+
 
 
     }
--- src/J03_KosulluIfadelerVeKodBloklari/C05_Pratik_HesapMakinesi.java
+++ src/J03_KosulluIfadelerVeKodBloklari/C05_Pratik_HesapMakinesi.java
@@ -1,7 +1,37 @@
 package J03_KosulluIfadelerVeKodBloklari;
 
+import java.util.Scanner;
+
 public class C05_Pratik_HesapMakinesi {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen ilk sayiyi giriniz ");
+        int n1 = input.nextInt();
+        System.out.println("Lutfen ikinci sayiyi giriniz ");
+        int n2 = input.nextInt();
+        System.out.println(" Lutfen islem seciniz \nToplama->1, \nCikarma->2, \nCarpma->3,  \nBolme->4 ");
+       int secim = input.nextInt();
+        switch (secim) {
+            case 1: System.out.println(" Toplam =  " +(n1 + n2));
+                break;
+            case 2:System.out.println(" Cikarma =  " +(n1 - n2));
+                break;
+            case 3:System.out.println(" Carpma =  " +(n1 * n2));
+                break;
+            case 4:
+                if (n2==0) {
+                    System.out.println("Bir sayi 'a bolunemez");
+                } else {
+                    System.out.println(" Bolme =  " + (n1 / n2));
+                }
+                break;
+
+            default:System.out.println(" Hatali giris yaptiniz ");
+
+        }
+
+
+
 
 
     }
