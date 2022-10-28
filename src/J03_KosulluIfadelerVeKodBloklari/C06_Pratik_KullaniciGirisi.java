28286fd8c490c1ce0001fbdeeeeeea5e36d655bc
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-28 14:17:15 +0300

    28.10.22

--- src/J03_KosulluIfadelerVeKodBloklari/C06_Pratik_KullaniciGirisi.java
+++ src/J03_KosulluIfadelerVeKodBloklari/C06_Pratik_KullaniciGirisi.java
@@ -1,8 +1,21 @@
 package J03_KosulluIfadelerVeKodBloklari;
 
+import java.util.Scanner;
+
 public class C06_Pratik_KullaniciGirisi {
     public static void main(String[] args) {
 
+        String userName, password;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kullanici Adiniz  :  ");
+        userName = scan.nextLine();
+        System.out.print("Sifreniz  :  ");
+        password = scan.nextLine();
+        if (userName.equals("patika") && password.equals("java123")){
+            System.out.println("Basari ile giris yaptiniz. Tebrikler!!! ");
+        }else {
+            System.out.println("Kullanici adiniz yada Sifreniz eslesmedi. Tekrar Deneyiniz!!!");
+        }
 
     }
 }
