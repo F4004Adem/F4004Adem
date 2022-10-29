87366e2faf880905f336becccbbb100b486241bf
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-29 19:22:21 +0300

    29.10.22

--- src/J03_KosulluIfadelerVeKodBloklari/C13_Pratik_ArtikYilHesaplama.java
+++ src/J03_KosulluIfadelerVeKodBloklari/C13_Pratik_ArtikYilHesaplama.java
@@ -3,11 +3,37 @@ package J03_KosulluIfadelerVeKodBloklari;
 import java.util.Scanner;
 
 public class C13_Pratik_ArtikYilHesaplama {
-    public static void main(String[] args) {
-       int maths , physics, chemistry, turkish, music;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Matematik Notnuz : ");
-        maths = scan.nextInt();
+    /*
+    Java ile kullanıcının girdiği yılın artık olup olmadığını bulan programı yazınız.
+Artık Yıl Nedir?
+Artık yıl, Miladî takvimde 365 yerine 366 günü olan yıl. Bu fazladan gün (artık gün),
+normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.
 
+Artık Yıl Nasıl Hesaplanır?
+Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin nedeni, bir astronomik
+yılın 365,25 gün değil, yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+Senaryo
+Yıl Giriniz : 2020
+2020 bir artık yıldır !
+Yıl Giriniz : 2021
+2021 bir artık yıldır değildir !
+     */
+
+
+    public static void main(String[] args) {
+       int yil;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lutfen Yil Giriniz : ");
+        yil = scan.nextInt();
+        if (yil%4==0 && yil % 100==0 && yil % 400==0) {
+            System.out.println("Girdiginiz yil  " + yil +" artik yildir ");
+        }else {
+            System.out.println("Girdiginiz yil  " + yil +" artik yil degildir ");
+        }
     }
 }
