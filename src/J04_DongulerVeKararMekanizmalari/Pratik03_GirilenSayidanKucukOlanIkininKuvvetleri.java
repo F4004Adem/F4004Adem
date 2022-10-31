9eddf843cf743cb06d28f338bad9a4e3523e6b0b
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-31 18:05:49 +0300

    31.10.22

--- src/J04_DongulerVeKararMekanizmalari/Pratik03_GirilenSayidanKucukOlanIkininKuvvetleri.java
+++ src/J04_DongulerVeKararMekanizmalari/Pratik03_GirilenSayidanKucukOlanIkininKuvvetleri.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Pratik03_GirilenSayidanKucukOlanIkininKuvvetleri {
     public static void main(String[] args) {
-        //ava döngüler ile girilen sayıya kadar olan
+        //Java döngüler ile girilen sayıya kadar olan
         // 2'nin kuvvetlerini ekrana yazdıran
         // programı yazıyoruz.
         int n;
             Scanner scan = new Scanner(System.in);
             System.out.print("Please enter an Integer ; ");
             n = scan.nextInt();
-            for(int i=1 ;i<=n ;i*=2){
+            for(int i=1 ;i<=n ;i*=2){// diger sayilarin kuvveti icin
+                // i*=2 ifadedeki 2 rakami degistirilip istenen sayi konulur
                 System.out.print(i+ ", ");
             }
 
