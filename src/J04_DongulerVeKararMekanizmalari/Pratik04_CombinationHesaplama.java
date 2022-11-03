5fe6306e716bcd5fcb6025055df0f0617a4b284d
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-03 18:36:48 +0300

    03.11.22

---
+++ src/J04_DongulerVeKararMekanizmalari/Pratik04_CombinationHesaplama.java
@@ -0,0 +1,30 @@
+package J04_DongulerVeKararMekanizmalari;
+
+import java.util.Scanner;
+
+//Java ile Combination  hesaplayan program yazıyoruz.
+public class Pratik04_CombinationHesaplama {
+    public static void main(String[] args) {
+        // C(n,r) = n! / (r! * (n-r)!)
+        int n,r, combination, nTotal=1, rTotal=1,  nMinusR=1;
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Number of object (n)  :");
+        n = scan.nextInt();
+        System.out.print("Number of choosing object (r)  :");
+        r = scan.nextInt();
+        int toplam =1;
+        for (int  i=1 ;i <=n ;i++){
+           nTotal *=i;
+        }
+       for (int j=1 ; j <=r ;j++){
+           rTotal *=j;
+       }
+        for (int k =1 ; k<=(n-r) ; k++){
+            nMinusR *=k;
+        }
+        combination =nTotal /(nTotal*nMinusR);
+        System.out.println("combination C ("+n+","+r+") = " + combination);
+
+    }
+
+}
--- src/J04_DongulerVeKararMekanizmalari/Pratik04_FaktoriyelHesaplama.java
+++ src/J04_DongulerVeKararMekanizmalari/Pratik04_FaktoriyelHesaplama.java
@@ -1,10 +1,22 @@
 package J04_DongulerVeKararMekanizmalari;
 
+import java.util.Scanner;
+//Java ile faktöriyel hesaplayan program yazıyoruz.
 public class Pratik04_FaktoriyelHesaplama {
     public static void main(String[] args) {
+        // n!= 1*2*3*4*....*n
+    Scanner scan= new Scanner(System.in);
+        System.out.print("Sayi giriniz faktoriyel icin  :");
+        int num = scan.nextInt();
+        int toplam =1;
+        for (int  i=1 ;i <=num ;i++){
+            toplam =toplam *i;
+        }
+        System.out.println("Faktoriyel = " + toplam);
 
 
 
 
     }
+
 }
