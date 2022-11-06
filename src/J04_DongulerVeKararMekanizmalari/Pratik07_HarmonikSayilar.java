20d2ada3902e99170c608239e264a09b6f264e67
ademalabas@gmail.com <ademalabas@gmail.com>
2022-11-06 23:43:55 +0300

    06.11.22

---
+++ src/J04_DongulerVeKararMekanizmalari/Pratik07_HarmonikSayilar.java
@@ -0,0 +1,33 @@
+package J04_DongulerVeKararMekanizmalari;
+
+import java.util.Scanner;
+
+public class Pratik07_HarmonikSayilar {
+    public static void main(String[] args) {
+/*
+ Java ile girilen sayının harmonik serisini bulan program yazacağız.
+
+Harmonik Seri Formülü :
+
+
+
+ */
+
+
+        Scanner input = new Scanner(System.in);
+        int sayi;
+        double harm = 0, unit = 0;
+
+        System.out.println("Sayınızı giriniz :");
+        sayi = input.nextInt();
+
+        for (int i = 1; i <=sayi; i++ ) {
+            unit = (1.0 / i);
+            harm += unit;
+        }
+        System.out.println(harm);
+
+
+
+    }
+}
